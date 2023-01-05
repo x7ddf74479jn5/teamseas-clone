aeac083377d0d4ae1ec3fee0f16d45c4ad680d65
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2023-01-05 15:44:33 +0900

    🚧 wip: init donation graphql

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,19 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { GraphQLModule } from "@nestjs/graphql";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { ApolloServerPluginLandingPageLocalDefault } from "apollo-server-core";
+import { DonationsModule } from './donations/donations.module';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+      typePaths: ["./**/*.graphql"],
+    }),
+    DonationsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
