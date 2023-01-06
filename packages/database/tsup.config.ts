e46de01c92b9853db37a6f2f502481a49e7c5cc4
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2023-01-06 20:22:26 +0900

    🗃️ update: prisma

--- packages/database/tsup.config.ts
+++ packages/database/tsup.config.ts
@@ -5,7 +5,7 @@ const isProduction = process.env.NODE_ENV === "production";
 export default defineConfig({
   clean: true,
   dts: true,
-  entry: ["src/index.ts"],
+  entry: ["src/client.ts", "src/nestjs-graphql.ts"],
   format: ["cjs", "esm"],
   minify: isProduction,
   sourcemap: true,
